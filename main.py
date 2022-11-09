2ed2add5c18578b72aff14cf2c0e3dbc32f0f9c1
xoma_star <40947642+xoma-star@users.noreply.github.com>
2022-11-09 06:16:49 +0000

    1

--- main.py
+++ main.py
@@ -1,61 +1,72 @@
 import numpy as np
-
-def target_func(x):
-    return np.power(x[0],2) + 2 * np.power(x[1],2)- 10 * np.power(x[2],2)
-
-def H(x):
-    return x[0] - np.power(x[1],2) + x[2] - 5
-
-def G(x):
-    return x[0] + 5 * x[1] + x[2] - 7
-
-def fi(x,r):# Метод внешних штрафов
-    return r/2 * ( np.power(H(x),2) + np.power(G(x),2))
-
-def P(x,r):
-    return target_func(x) + fi(x,r)
-
-def grad_target_func(x):
-    return np.array([2*x[0], 4*x[1], 20*x[2]])
-
-def grad_H(x):
-    return np.array([1, 2*x[1], 1])
-
-def grad_G():
-    return np.array([1, 5, 1])
-
-def grad_P(x,r):
-    return grad_target_func(x) + ( r*H(x)*grad_H(x) + r*G(x)*grad_G() )
-
-eps_1 = 0.001
-eps_2 = 0.01
-M = 500 # max iters
-
-C = 4
-r = 0.1
-
-while True:
-    x = np.array([0,0,0])
-    x_prev = np.array([1,1,1])
-
-    t_k = 1
-    dots = []
-
-    while (np.linalg.norm(x - x_prev) > eps_2) and (len(dots) < M):
-
-        x_prev = x
-        x = x - t_k*grad_P(x,r)
-        if P(x,r) - P(x_prev,r) > 0:
-            t_k = t_k/2
-        
-        dots.append(x)
-
-    if abs(fi(x,r)) <= eps_1:
-        break
-    else:
-        r = r * C
+import sympy
 
 
-print('iter num: ', dots.__len__())
-print('Решение: ', x)
-print('Значние функции в точке x:', target_func(x))
+
+def gradientDescent(functions, x0=(2, 2, 2), M=10000, eps1=0.001, alfa=0.01, C=1.03, r=0.01):
+    x1, x2, x3 = x0
+
+    dY_divide_dX1, dY_divide_dX2, dY_divide_dX3, _, _, phi, p, y = functions()
+    dY_divide_dX1, dY_divide_dX2, dY_divide_dX3, phi, p, y = (sympy.lambdify([sympy.Symbol('x1'), sympy.Symbol('x2'), sympy.Symbol('x3'), sympy.Symbol('r')], i) for i in (dY_divide_dX1, dY_divide_dX2, dY_divide_dX3, phi, p, y))
+
+    numberOfIteration = 0
+    rNext = r
+    while(numberOfIteration < M):
+        f = y(x1, x2, x3, r)
+        phi1 = phi(x1, x2, x3, r)
+        p1 = p(x1, x2, x3, r)
+
+        x1Next = x1 - alfa * dY_divide_dX1(x1, x2, x3, rNext)
+        x2Next = x2 - alfa * dY_divide_dX2(x1, x2, x3, rNext)
+        x3Next = x3 - alfa * dY_divide_dX3(x1, x2, x3, rNext)
+
+        fNext = y(x1Next, x2Next, x3Next, r)
+        phi2 = phi(x1Next, x2Next, x3Next, rNext)
+        p2 = p(x1Next, x2Next, x3Next, rNext)
+
+        if(phi1 < eps1):
+            print(f'phi1 = {phi1} p2 = {p2}')
+            break
+        else:
+            x1 = x1Next
+            x2 = x2Next
+            x3 = x3Next
+            rNext = C * rNext
+            # alfa *= 0.95
+            numberOfIteration += 1
+
+    return x1Next, x2Next, x3Next, fNext, y(*x0, r)
+
+def derivativesFunction():
+    x1 = sympy.Symbol('x1')
+    x2 = sympy.Symbol('x2')
+    x3 = sympy.Symbol('x3')
+
+    r = sympy.Symbol('r')
+
+    # defenitions
+    y = x1 ** 2 + 2 * x2 ** 2 + 10 * (x3 ** 2)
+    cond1 = x1 - x2 ** 2 + x3 - 5
+    cond2 = x1 + 5 * x2 + x3 - 7
+
+    phi = r / 2 * (cond1 ** 2 + cond2 ** 2)
+    p = y + phi
+
+    # print(dY_divide_dX1)
+    # print(dY_divide_dX2)
+    # print(dY_divide_dX3)
+
+    return p.diff(x1), p.diff(x2), p.diff(x3), cond1, cond2, phi, p, y
+
+def task1Variant10(functions=derivativesFunction):
+
+    return gradientDescent(functions, x0=(0, 0, 0))
+
+
+x1min, x2min, x3min, fmin, fold = task1Variant10()
+
+print(f'x1min = {x1min}')
+print(f'x2min = {x2min}')
+print(f'x3min = {x3min}')
+print(f'fmin = {fmin}')
+print(f'fold = {fold}')
